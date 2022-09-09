82935de3b137434a8375f0855c985aff929f5009
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-09-09 16:44:06 +0300

    footer comp and gallery slider

--- ghm-react/src/Components/Footer.jsx
+++ ghm-react/src/Components/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BsArrowUpCircleFill } from "react-icons/bs";
 
 function Footer() {
-  return <section className="footer">Footer</section>;
+  return (
+    <section className="footer">
+      <div className="footer-left">
+        <div className="arrow-up">
+          <a href="#home">
+            <BsArrowUpCircleFill className="up-arrow-svg" />
+          </a>
+        </div>
+      </div>
+      <div className="footer-right">
+        <div className="social-links">link here</div>
+        <div>created by me</div>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
--- ghm-react/src/Components/Gallery.jsx
+++ ghm-react/src/Components/Gallery.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import gallerySVG from "./Pictures/undraw-gallery.svg";
+import img1 from "./Pictures/snowboarders.jpg";
+import img2 from "./Pictures/gallery-01.jpg";
+import img3 from "./Pictures/gallery-02.jpg";
+import img4 from "./Pictures/gallery-03.jpg";
+import img5 from "./Pictures/gallery-04.jpg";
+import img6 from "./Pictures/gallery-05.jpg";
+import img7 from "./Pictures/gallery-06.jpg";
+import img8 from "./Pictures/gallery-08.jpg";
+import img9 from "./Pictures/gallery-09.jpg";
+import img10 from "./Pictures/gallery-11.jpg";
 
-function Gallery() {
+export default function Gallery() {
   return (
     <section className="gallery">
       <div className="rounded-container glr">
         <span className="hash" id="gallery"></span>
         <h1>GALLERY</h1>
-        <div className="slider">slider</div>
+        <Slider
+          className="slider"
+          images={[img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]}
+        >
+          slider
+        </Slider>
       </div>
       <div className="image">
         <img alt="gallery" src={gallerySVG} />
@@ -16,4 +31,31 @@ function Gallery() {
   );
 }
 
-export default Gallery;
+/* SLIDER */
+export function Slider({ images }) {
+  const [current, setCurrent] = useState(0);
+
+  /* FUNCTIONS */
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  return (
+    <div className="slider">
+      <img alt="" className="main-img" src={images[current]} />
+      {current !== 0 && (
+        <button className="prev-btn" onClick={prev}>
+          prev
+        </button>
+      )}
+      {current !== images.length - 1 && (
+        <button className="next-btn" onClick={next}>
+          next
+        </button>
+      )}
+    </div>
+  );
+}
