29a4e9a0518e48250fca4db888fdd0bb81e6cf78
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-09-11 16:09:43 +0300

    contacts emailjs implementation

--- ghm-react/src/Components/Contact.jsx
+++ ghm-react/src/Components/Contact.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import { SiMinutemailer } from "react-icons/si";
 
 function Contact() {
+  /* emailjs code*/
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_a0zsakn",
+        "template_l8p28wx",
+        form.current,
+        "g4PHWosCI3QMWwFrf"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section className="contact">
       <span className="hash" id="contact"></span>
@@ -15,11 +39,32 @@ function Contact() {
           </div>
 
           <div className="contact-form">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-              dolorem quae inventore laboriosam animi voluptate?
-            </p>
-            <div className="form">form</div>
+            <div className="form">
+              <form ref={form} onSubmit={sendEmail} className="contact-right">
+                <h1>Get In Touch</h1>
+                <div>
+                  <SiMinutemailer /> gergely.gimesi@gmail.com
+                </div>
+                <div className="contact-right-name">
+                  <input type="text" name="fistname" placeholder="First Name" />
+                  <input type="text" name="lastname" placeholder="Last Name" />
+                </div>
+                <div className="contact-right-email-phone">
+                  <input type="email" name="email" placeholder="Email" />
+                  <input type="tel" name="phone" placeholder="Phone No." />
+                </div>
+                <div className="contact-right-message">
+                  <textarea name="message" placeholder="Message" />
+                </div>
+                <div className="contact-right-send">
+                  <input
+                    className="contact-right-send-input"
+                    type="submit"
+                    value="Send"
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
